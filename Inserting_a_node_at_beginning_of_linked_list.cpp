0b326ec8e2557995a85299d32e04d276261c533a
Koustubh-lapate <73625548+Koustubh-lapate@users.noreply.github.com>
2021-12-17 19:40:10 +0530

    Create Inserting_a_node_at_beginning_of_linked_list.cpp

---
+++ Inserting_a_node_at_beginning_of_linked_list.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Linkedlist{
+    struct Node{
+        int data;
+        Node* next;
+        Node(int d){
+            data = d;
+            next = NULL;
+        }
+    };
+    
+    Node *head, *tail;
+    
+public:
+    Linkedlist(){
+        head = NULL;
+        tail = NULL;
+    }
+    
+    void insertatend(int data){
+        Node *newNode = new Node(data);
+        
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = tail->next;
+        }
+    }
+    
+    void insertatbeginning(int data){
+        Node *newNode = new Node(data);
+        
+        if(head==NULL){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    
+    void print(){
+        if(head == NULL){
+            cout<<"Empty Linkedlist\n";
+        }
+        else{
+            Node* current = head;
+            while(current!=NULL){
+                cout<<current->data<<"->";
+                current = current->next;
+            }
+            cout<<"NULL\n";
+        }
+    }
+};
+
+int main() {
+    Linkedlist l = Linkedlist();
+    l.insertatend(0);
+    l.insertatend(1);
+    l.insertatend(2);
+    l.insertatend(3);
+    l.insertatend(4);
+    l.insertatend(5);
+    l.insertatbeginning(100);
+    l.insertatbeginning(200);
+    l.print();
+	return 0;
+}
